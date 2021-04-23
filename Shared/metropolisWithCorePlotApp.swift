e97bc062fae38ec3bfdcaecc679889ffa3ba93f0
Matt <madas@hawk.iit.edu>
2021-04-23 17:59:43 -0500

    Initial Commit

---
+++ Shared/metropolisWithCorePlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  metropolisWithCorePlotApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct metropolisWithCorePlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
